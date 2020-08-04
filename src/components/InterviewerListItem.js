5b72d7c757546ed45d40cd91b679562ee25aa3e3
robpolisena <robpolisena@gmail.com>
2020-08-04 15:38:36 -0400

    Refeactored interviewerlist and interviewerListItem components to not pass the id propand building the Appointment component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -2,25 +2,22 @@ import React from "react";
 import classNames  from "classnames";
 import "components/InterviewerListItem.scss";
 
-
 export default function InterviewerListItem(props) {
 
   const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": props.selected
  });
 
-
   return (
     <li className={interviewerClass}
-      onClick={() => props.setInterviewer(props.name)}
+      onClick={props.setInterviewer}
       >
     <img
     className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
+    src={props.avatar}
+    alt={props.name}
   />
-  Sylvia Palmer
+  {props.selected && props.name}
 </li>
 )
-
 }
