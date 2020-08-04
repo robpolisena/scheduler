5832ce14382023c0573d259fbeb314a33bef12de
robpolisena <robpolisena@gmail.com>
2020-08-04 19:13:44 -0400

    built the components: Header, Empty, Show, Confirm, Status, Error

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,10 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -134,8 +138,12 @@ storiesOf("Button", module)
       storiesOf("Appointment", module)
         .addParameters({
           backgrounds: [{ name: "white", value: "#fff", default: true }]
-           })
-           .add("Appointment", () => <Appointment />)
-           .add("Appointment with Time", () => <Appointment time="12pm" />);
-           .add("Header", () => <Header time="12pm" />);
-           .add("Empty", () => (  <Empty onAdd={action("onAdd")} />)
+          })
+        .add("Appointment", () => <Appointment />)
+        .add("Header", () => <Header time="12pm" />)
+        .add("Empty", () =>  <Empty onAdd={action("onAdd")} />)
+        .add("Show", () =>  <Show interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
+        .add("Confirm", () =>  <Confirm onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
+        .add("Status", () =>  <Status message="Deleting" />)
+        .add("Error", () =>  <Error onClose={action("onClose")} message="Could not delete appointment." />)
+      
