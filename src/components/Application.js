5bb9b05f2cc3e605e6a397f48b76369ac6b59f60
robpolisena <robpolisena@gmail.com>
2020-08-05 16:57:12 -0400

    added useState for setting days and added axios to request api/days to update the days state

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
+import axios from 'axios';
 
 import "components/Application.scss";
 
@@ -59,31 +60,19 @@ const appointments = [
   },
 ];
 
+
 export default function Application(props) {
-
   
-
-
-
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
-
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:8001/api/days`).then((response) => {
+      console.log('useEffect has been called',response.data[0].name)
+      setDays(response.data)
+      console.log(days);
+    })
+  }, [])
 
   return (
     <main className="layout">
