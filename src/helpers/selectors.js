6fa71e9082dfed213dee06bfd34fcbcf7fcec70e
robpolisena <robpolisena@gmail.com>
2020-08-05 19:20:46 -0400

    created the getAppointmentForDay selector function to return array of appointments for a given day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,26 @@
+export function getAppointmentsForDay(state, day) {
+
+  //retrieve the days object based on day name
+  const matchDays = state.days.filter(days => days.name === day)
+
+  // if day is not found
+  if(matchDays.length === 0) {
+   return [];
+  }
+
+  // iterate through appointment array for the given day
+  const appt = matchDays[0].appointments 
+  const appts = state.appointments[appt]
+  const appoints = [];
+  for(let a of appt) {
+    appoints.push(state.appointments[a])
+  }
+  return appoints
+}
+ 
+
+  
+  
+
+
+
